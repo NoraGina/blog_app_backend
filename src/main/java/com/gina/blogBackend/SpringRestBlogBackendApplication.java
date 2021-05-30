a88e7a33d7690518881996b2f594c027a6209d82
Gina <gina.agapescu@gmail.com>
2021-05-30 07:38:36 +0300

    First commit creat model entity for user

---
+++ src/main/java/com/gina/blogBackend/SpringRestBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gina.blogBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestBlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gina/blogBackend/SpringRestBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gina.blogBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
