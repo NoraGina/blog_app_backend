8ce1d7ae261bba6026b5e7bec542b70cdcab312a
Gina <gina.agapescu@gmail.com>
2021-05-30 08:35:54 +0300

    Adding controller and response for signUp and signIn

---
+++ src/main/java/com/gina/blogBackend/controller/UserController.java
@@ -0,0 +1,110 @@
+package com.gina.blogBackend.controller;
+
+import com.gina.blogBackend.model.ERole;
+import com.gina.blogBackend.model.Role;
+import com.gina.blogBackend.model.User;
+import com.gina.blogBackend.payload.dto.LoginRequest;
+import com.gina.blogBackend.payload.dto.SignUpRequest;
+import com.gina.blogBackend.payload.response.JwtResponse;
+import com.gina.blogBackend.payload.response.MessageResponse;
+import com.gina.blogBackend.repository.RoleRepository;
+import com.gina.blogBackend.repository.UserRepository;
+import com.gina.blogBackend.security.JwtUtils;
+import com.gina.blogBackend.service.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    PasswordEncoder encoder;
+
+    @PostMapping("/signIn")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest){
+        if(userRepository.existsByUsername(signUpRequest.getUsername())){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+        if(userRepository.existsByEmail(signUpRequest.getEmail())){
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role>roles = new HashSet<>();
+        if(strRoles == null){
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(()-> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        }else{
+            strRoles.forEach(role ->{
+                switch (role){
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(()-> new RuntimeException("Error: Role is not found"));
+                        roles.add(userRole);
+                }
+            });
+        }
+        user.setRoles(roles);
+        userRepository.save(user);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
