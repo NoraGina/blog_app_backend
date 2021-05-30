81159095dc2118286fc7c4d8148990024f05de33
Gina <gina.agapescu@gmail.com>
2021-05-30 07:47:49 +0300

    Create User repository

---
+++ src/main/java/com/gina/blogBackend/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.gina.blogBackend.repository;
+
+import com.gina.blogBackend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
